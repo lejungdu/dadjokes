1d36e80a379811385ddef60dd2e38bd80bd11c41
Le-Jung Du <lejung@qualias.jp>
2020-09-10 18:28:12 +0900

    2020/09/10

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>Jokes</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            jokes: [],
+        }
+    },
+    async created(){
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
+        }
+        const url = 'https://icanhazdadjoke.com/search'
+        
+
+        try {
+            const res = await axios.get(url, config);
+            console.log('hi')
+            console.log(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+
+    },
+    head(){
+        return{
+            title: 'Welcome to Dad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for corny dad jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
